5000685dd7390eaa5bd05142dbc4682cfb228e63
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-04-28 01:12:11 +0530

    delete build directory

--- build/graphql/resolvers/user.js
+++
@@ -1,8 +0,0 @@
-const { getUserProfile } = require('../../helpers/user');
-module.exports = {
-    User: {
-        profile: user => {
-            return getUserProfile(user._id);
-        }
-    }
-};
