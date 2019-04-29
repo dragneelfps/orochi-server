b6525dfa86570cce34657fce709bc318e2387db5
Sourabh S. Rawat <dragneelfps@gmail.com>
2019-04-29 10:10:50 +0530

    add mocha + chai testing support

---
+++ tests/example.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+
+const dbRead = async (input: number): Promise<number> => {
+  return input;
+}
+
+describe('db', function () {
+  let dbVal: number;
+  before(async function () {
+    dbVal = await dbRead(1);
+    console.log('hellow')
+  })
+
+  it("read val is 1", function () {
+    expect(dbVal).to.equal(1);
+  })
+
+  it("read val is not 12", function () {
+    expect(dbVal).to.not.equal(12);
+  })
+
+})
